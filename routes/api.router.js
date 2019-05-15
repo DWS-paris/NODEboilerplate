7a2c5bb2e25337b8efbea172e423207c944cc824
DWS-paris <j.noyer@digitalworkshop.fr>
2019-05-15 15:49:07 +0200

    Ajout des méthodes CRUD API

--- routes/api.router.js
+++ routes/api.router.js
@@ -7,10 +7,47 @@ Configurer le module de route
 //
 
 /*
-Définition des routes
+Définition du CRUD
 */
-    router.get('/', (req, res) => {
-        res.json({ msg: 'Hello API' })
+    // Create Item: POST
+    router.post('/article', (req, res) => {
+
+        /* 
+            Pour créer un article il faut une valeur pour :
+            - title
+            - content
+        */
+
+
+        res.json({ msg: 'Create Article' })
+    });
+
+    // Read all Items: GET
+    router.get('/article', (req, res) => {
+        res.json({ msg: 'Read all Article' })
+    });
+
+    // Read one Item: GET
+    router.get('/article/:id', (req, res) => {
+        res.json({ msg: 'Read one Article' })
+    });
+
+    // Update one Item: PUT
+    router.put('/article/:id', (req, res) => {
+
+        /* 
+            Pour éditer un article il faut une valeur pour :
+            - title
+            - content
+        */
+
+
+        res.json({ msg: 'Update one Article' })
+    });
+
+    // Delete one Item: DELETE
+    router.delete('/article/:id', (req, res) => {
+        res.json({ msg: 'Delete one Article' })
     });
 //
 
